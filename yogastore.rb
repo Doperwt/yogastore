77674f1f50ae523cd3dea9bbc032cea4183901c3
Robert den Ouden <r.f.denouden@gmail.com>
2018-01-10 15:36:42 +0100

    changed methods and ending phrase

--- yogastore.rb
+++ yogastore.rb
@@ -1,15 +1,32 @@
-@shopping_cart = []
-@products = [
-  { reference_number: 1231, name: "Super Lite Mat", price: 10 },
-  { reference_number: 1232, name: "Power Mat", price: 20 },
-  { reference_number: 1233, name: "Block", price: 30 },
-  { reference_number: 1234, name: "Meditation cushion", price: 30 },
-  { reference_number: 1235, name: "The best T-shirt", price: 200 },
-  { reference_number: 1236, name: "The cutest yoga pants", price: 300 },
-  { reference_number: 1237, name: "Bring Yoga To Life", price: 30 },
-  { reference_number: 1238, name: "Light On Yoga", price: 10 }
-]
-@price = 0
+def initialize_yogastore
+  @shopping_cart = []
+  @products = [
+    { reference_number: 1231, name: "Super Lite Mat", price: 10 },
+    { reference_number: 1232, name: "Power Mat", price: 20 },
+    { reference_number: 1233, name: "Block", price: 30 },
+    { reference_number: 1234, name: "Meditation cushion", price: 30 },
+    { reference_number: 1235, name: "The best T-shirt", price: 200 },
+    { reference_number: 1236, name: "The cutest yoga pants", price: 300 },
+    { reference_number: 1237, name: "Bring Yoga To Life", price: 30 },
+    { reference_number: 1238, name: "Light On Yoga", price: 10 }
+  ]
+  @price = 0
+  puts "Hello user, welcome to the Yoga Store"
+  list_products
+  loop do
+    print  "choose a product by its reference number or write end to complete : "
+    input = gets.chomp
+    case input
+    when  'end'
+      puts "Thanks for shopping at the Yogastore!!!"
+      break
+    else
+      display_add_cart(input)
+    end
+  end
+end
+
+
 def display_add_cart(input)
   @products.each do |product|
     if product[:reference_number] == input.to_i
@@ -19,30 +36,22 @@ def display_add_cart(input)
       break
     end
   end
+  puts "--------------------\n" #lines for readability
   display_cart
   puts "--------------------\ntotal : #{@price}"
 end
-def display_cart
+
+def display_cart #displaying the cart for the customer
   @shopping_cart.each do |item|
     puts "#{item[:name]}, price : #{item[:price]}"
   end
 end
-puts "Hello user, welcome to the Yoga Store"
 
-@products.each do |product|
-  puts "#{product[:reference_number]}: #{product[:name]} price is #{product[:price]}"
-end
-loop do
-  print  "choose a product by its reference number or write end to complete : "
-
-  input = gets.chomp
-
-  case input
-  when  'end'
-    puts "you entered nothing"
-    break
-  else
-    display_add_cart(input)
+def list_products
+  @products.each do |product|
+    puts "#{product[:reference_number]}: #{product[:name]} price is #{product[:price]}"
   end
 end
+
+initialize_yogastore
 p @shopping_cart
